e329fee11b9770e78e8d6cbe19728416da304096
xperimentalone <sklaisuki@gmail.com>
2025-08-29 22:14:04 +0800

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Route, Switch } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
